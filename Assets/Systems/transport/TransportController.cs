9eba3d32d73a4f44e14a3087c9467fdd888ef868
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2025-11-01 02:33:55 +0100

    refactor transport manager to exclude visual logic

--- Assets/Systems/transport/TransportController.cs
+++ Assets/Systems/transport/TransportController.cs
@@ -10,15 +10,9 @@ namespace Systems.Transport
         public static TransportController Instance { get; private set; }
         public TransportManager Manager => transportManager;
 
-        [SerializeField] private HexGrid grid;
-        [SerializeField] private SmoothLineRenderer smoothLineRendererPrefab;
         [SerializeField] private PathfindingController pathfindingController;
-        [SerializeField] private float offsetHeight = 0.6f;
         private TransportManager transportManager;
 
-        private Dictionary<Guid, SmoothLineRenderer> transportRouteVisualizers = new();
-        private SmoothLineRenderer previewedRoute;
-        private Guid? selectedRouteId = null;
 
         void Awake()
         {
@@ -33,7 +27,6 @@ namespace Systems.Transport
         public void Initialize()
         {
             transportManager = new TransportManager(pathfindingController, new Dictionary<Guid, TransportRoute>());
-            previewedRoute = Instantiate(smoothLineRendererPrefab, transform);
         }
 
         public TransportRoute CreateRoute(WorldNode origin, WorldNode destination)
@@ -45,73 +38,15 @@ namespace Systems.Transport
         public bool TryDeleteRoute(Guid routeId)
         {
             transportManager.RemoveRoute(routeId);
-            if (transportRouteVisualizers.TryGetValue(routeId, out SmoothLineRenderer visualizer))
-            {
-                Destroy(visualizer.gameObject);
-            }
-            transportRouteVisualizers.Remove(routeId);
-            if (selectedRouteId == routeId)
-            {
-                selectedRouteId = null;
-            }
             return true;
         }
 
-        public bool TryCreateRoute(WorldNode origin, WorldNode destination)
+        public bool TryCreateRoute(WorldNode origin, WorldNode destination, out TransportRoute route)
         {
-            previewedRoute.HideLine();
-            if (selectedRouteId != null)
-            {
-                transportRouteVisualizers[selectedRouteId.Value].HideLine();
-            }
-
-            TransportRoute route = transportManager.CreateRoute(origin, destination);
+            route = transportManager.CreateRoute(origin, destination);
             if (route == null) return false;
 
-            SmoothLineRenderer lineRenderer = Instantiate(smoothLineRendererPrefab);
-            transportRouteVisualizers[route.Id] = lineRenderer;
-            lineRenderer.RenderLine(route.path.ConvertAll(p => grid.Grid.CellToWorld(p.ToOffset())).ConvertAll(v => new Vector3(v.x, offsetHeight, v.z)));
-            lineRenderer.ShowLine();
-            selectedRouteId = route.Id;
-
             return true;
-        }
-
-        public void PreviewRoute(WorldNode origin, WorldNode destination)
-        {
-            if (selectedRouteId != null)
-            {
-                transportRouteVisualizers[selectedRouteId.Value].HideLine();
-                selectedRouteId = null;
-            }
-
-            if (!transportManager.CanCreateRoute(origin, destination, out string errorMessage, out List<HexCoordinate> path))
-            {
-                previewedRoute.ChangeColor(Color.red);
-                Debug.Log($"Cannot commit route: {errorMessage}");
-            }
-            else
-            {
-                previewedRoute.ChangeColor(Color.green);
-            }
-
-            previewedRoute.RenderLine(path.ConvertAll(p => grid.Grid.CellToWorld(p.ToOffset())).ConvertAll(v => new Vector3(v.x, offsetHeight, v.z)));
-            previewedRoute.ShowLine();
-        }
-
-        public void SelectRoute(Guid routeId)
-        {
-            previewedRoute.HideLine();
-            if (selectedRouteId != null)
-            {
-                transportRouteVisualizers[selectedRouteId.Value].HideLine();
-            }
-
-            if (transportRouteVisualizers.TryGetValue(routeId, out SmoothLineRenderer visualizer))
-            {
-                visualizer.ShowLine();
-                selectedRouteId = routeId;
-            }
         }
     }
 }
--- Assets/Systems/ui/TransportUIController.cs
+++ Assets/Systems/ui/TransportUIController.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Systems.Grid;
 using Systems.Transport;
 using UnityEngine;
 
@@ -6,14 +9,25 @@ namespace Systems.UI
 {
     public class TransportUIController : MonoBehaviour, IUIController
     {
-        [SerializeField] private TransportController transportController;
         private TransportRoutePanel transportRoutePanel;
         public IUIModeSegment UIModeSegment => transportRoutePanel;
+
+        [SerializeField] private TransportController transportController;
+
+        [Header("Transport Path Visualization")]
+        [SerializeField] private HexGrid grid;
+        [SerializeField] private SmoothLineRenderer smoothLineRendererPrefab;
+        [SerializeField] private PathfindingController pathfindingController;
+        [SerializeField] private float offsetHeight = 0.6f;
+
+        private readonly Dictionary<Guid, SmoothLineRenderer> transportRouteVisualizers = new();
+        private SmoothLineRenderer previewedRoute;
+        private Guid? selectedRouteId = null;
 
         public void Initialize()
         {
             transportController.Initialize();
-
+            previewedRoute = Instantiate(smoothLineRendererPrefab, transform);
             transportRoutePanel = new(transportController.Manager.GetAllRoutes());
             transportRoutePanel.OnRouteDeleted += HandleRouteDeletion;
             transportRoutePanel.OnCreateRouteConfirmed += HandleRouteCreation;
@@ -45,7 +59,7 @@ namespace Systems.UI
             else
             {
                 transportRoutePanel.SetDestinationNode(node);
-                transportController.PreviewRoute(origin, node);
+                PreviewRoute(origin, node);
             }
 
             if (isClick && origin != null && destination != null)
@@ -54,21 +68,78 @@ namespace Systems.UI
             }
         }
 
-        private void HandleRouteDeletion(Guid guid)
+        private void PreviewRoute(WorldNode origin, WorldNode destination)
         {
-            transportController.TryDeleteRoute(guid);
+            if (selectedRouteId != null)
+            {
+                transportRouteVisualizers[selectedRouteId.Value].HideLine();
+                selectedRouteId = null;
+            }
+
+            if (!transportController.Manager.CanCreateRoute(origin, destination, out string errorMessage, out List<HexCoordinate> path))
+            {
+                previewedRoute.ChangeColor(Color.red);
+                Debug.Log($"Cannot commit route: {errorMessage}");
+            }
+            else
+            {
+                previewedRoute.ChangeColor(Color.green);
+            }
+
+            previewedRoute.RenderLine(path.ConvertAll(p => grid.Grid.CellToWorld(p.ToOffset())).ConvertAll(v => new Vector3(v.x, offsetHeight, v.z)));
+            previewedRoute.ShowLine();
+        }
+
+        private void HandleRouteDeletion(Guid id)
+        {
+            transportController.TryDeleteRoute(id);
+            if (transportRouteVisualizers.TryGetValue(id, out SmoothLineRenderer visualizer))
+            {
+                Destroy(visualizer.gameObject);
+            }
+            transportRouteVisualizers.Remove(id);
+            if (selectedRouteId == id)
+            {
+                selectedRouteId = null;
+            }
         }
         private void HandleRouteCreation(WorldNode origin, WorldNode destination)
         {
-            transportController.TryCreateRoute(origin, destination);
+            previewedRoute.HideLine();
+            if (selectedRouteId != null)
+            {
+                transportRouteVisualizers[selectedRouteId.Value].HideLine();
+            }
+
+            if (!transportController.TryCreateRoute(origin, destination, out TransportRoute route)) return;
+
+            SmoothLineRenderer lineRenderer = Instantiate(smoothLineRendererPrefab, transform);
+            transportRouteVisualizers[route.Id] = lineRenderer;
+            lineRenderer.RenderLine(route.path.ConvertAll(p => grid.Grid.CellToWorld(p.ToOffset())).ConvertAll(v => new Vector3(v.x, offsetHeight, v.z)));
+            lineRenderer.ShowLine();
+            selectedRouteId = route.Id;
+
         }
         private void HandleRouteSelection(TransportRoute route)
         {
-            transportController.SelectRoute(route.Id);
+            previewedRoute.HideLine();
+            if (selectedRouteId != null)
+            {
+                transportRouteVisualizers[selectedRouteId.Value].HideLine();
+            }
+
+            if (transportRouteVisualizers.TryGetValue(route.Id, out SmoothLineRenderer visualizer))
+            {
+                visualizer.ShowLine();
+                selectedRouteId = route.Id;
+            }
         }
 
         public void Exit()
         {
+            transportRouteVisualizers.Values.ToList().ForEach(v => v.HideLine());
+            previewedRoute.HideLine();
+            selectedRouteId = null;
             transportRoutePanel.ExitMode();
         }
 
